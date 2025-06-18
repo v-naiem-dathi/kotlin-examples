ab16b30704fbbe98912cbb5c03d978b531d06b77
Naiem Dathi <naiem.dathi@hippodigital.co.uk>
2025-06-18 09:58:40 +0100

    Added Functional Examples

---
+++ src/org/kotlinlang/play/functional/higherorderfunctions/Return.kt
@@ -0,0 +1,12 @@
+package org.kotlinlang.play.functional.higherorderfunctions
+
+fun operation(): (Int) -> Int {                                     // 1
+    return ::square
+}
+
+fun square(x: Int) = x * x                                          // 2
+
+fun main() {
+    val func = operation()                                          // 3
+    println(func(2))                                                // 4
+}
