030692b6b63b762a660dff0d3d76f80bebecb248
Naiem Dathi <naiem.dathi@hippodigital.co.uk>
2025-06-23 10:05:54 +0100

    Added Delegation Examples

---
+++ src/org/kotlinlang/play/delegation/properties/Map.kt
@@ -0,0 +1,15 @@
+package org.kotlinlang.play.delegation.properties
+
+class User(val map: Map<String, Any?>) {
+    val name: String by map                // 1
+    val age: Int     by map                // 1
+}
+
+fun main() {
+    val user = User(mapOf(
+        "name" to "John Doe",
+        "age"  to 25
+    ))
+
+    println("name = ${user.name}, age = ${user.age}")
+}
